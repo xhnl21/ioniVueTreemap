5d0bcc445ec439ca22e402cc449abe29f532d073
Xavier <xhnl21@gmail.com>
2024-07-04 21:11:01 -0400

    freemap

---
+++ src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <chart /> -->
+  <treemap />
+</template>
+
+<script>
+import Chart from "../components/chart.vue";
+import treemap from "../components/treemap.vue";
+export default {
+  name: "app",
+  components: {
+    Chart,
+    treemap
+  },
+  methods: {
+    handler() {
+      var args = arguments;
+      for (var arg of args) {
+        if (arg instanceof Function) {
+          arg();
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+#container {
+  min-width: 300px;
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
